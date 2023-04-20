cc33d4060aa936815cccf9d4761ba8c28e8806f5
Dani <daniyil.kud@gmail.com>
2023-04-20 21:03:42 +0200

    add recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test of recipe',
+      'https://www.wholesomeyum.com/wp-content/uploads/2021/11/wholesomeyum-London-Broil-Recipe-In-The-Oven-21.jpg'
+    ),
+  ];
 }
